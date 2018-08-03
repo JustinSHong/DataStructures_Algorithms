0d1a0e892d7813e40ffde95770f4d726935c8b9d
JustinSHong <jhong2009@gmail.com>
2018-08-02 21:06:35 -0700

    Queue_Array.js: created nodes/queue and added nodes to queue

--- Queue/Queue_Array.js
+++ Queue/Queue_Array.js
@@ -11,39 +11,70 @@ class Queue {
 
 	// check if queue is empty
 	isEmpty() {
-		console.log(this.collection.length);
+		console.log(`isEmpty ${this.collection.length}`);
 		return this.collection.length === 0 ? true : false;
 	}
 
 	// return node at the start of the queue
 	peek() {
 		const head = this.collection[0];
-		console.log(head);
+		console.log(`peek: ${head.data}`);
 		return head;
 	}
 
 	// add node to end of queue
 	enqueue(node) {
 		this.collection.push(node);
-		console.log(node);
+		console.log(`enqueue: ${node.data}`);
 		return node; // show node that was added
 	}
 
 	// remove node at the start of queue
 	dequeue() {
 		const removed = this.collection.shift();
-		console.log(removed);
+		console.log(`dequeue: ${removed.data}`);
 		return removed; // show node that was removed
 	}
 
 	// show size of the queue
 	size() {
-		console.log(this.collection.length);
+		console.log(`size: ${this.collection.length}`);
 		return this.collection.length;
 	}
 
 	// print all nodes in the queue
 	show() {
-		console.log(this.collection);
+		console.log(`collection: ${this.collection}`);
 	}
 }
+
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node("Fred");
+
+const queue = new Queue();
+
+queue.show();
+queue.size();
+queue.isEmpty();
+
+queue.enqueue(node1);
+queue.enqueue(node2);
+queue.enqueue(node3);
+queue.enqueue(node4);
+queue.enqueue(node5);
+
+queue.show();
+queue.size();
+queue.isEmpty();
+queue.peek();
+queue.dequeue();
+queue.dequeue();
+queue.dequeue();
+queue.peek();
+queue.show();
+queue.size();
+queue.isEmpty();
+
